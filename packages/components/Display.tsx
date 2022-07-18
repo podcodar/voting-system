a5735b3f05e75657bedad894258ce36a4796f049
frattezi <pfrattezi@gmail.com>
2022-07-18 20:16:24 -0300

    feat: add e example of context usage

--- packages/components/Display.tsx
+++ packages/components/Display.tsx
@@ -1,8 +1,17 @@
+import { useEffect } from 'react';
 import { Flex, Grid } from '@chakra-ui/react';
+
+import { useVotingContext } from '@packages/features/voting-context';
 
 import PartyInfo from './PartyInfo';
 
 export default function Display() {
+  const { partys } = useVotingContext();
+
+  useEffect(() => {
+    console.log(JSON.stringify(partys));
+  }, [partys]);
+
   return (
     <Grid templateColumns="1fr 1fr" h="80%" alignSelf="center">
       <Flex bg="#FED7D7" h="100%" justifyContent="center" alignItems="end">
