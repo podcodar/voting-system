52e1add06d47ed8255c08bda2fcc804e5c99e9dd
vzsoares <jogolots2@gmail.com>
2022-07-20 22:25:29 -0300

    remove mock initial state

--- packages/features/voting-context.tsx
+++ packages/features/voting-context.tsx
@@ -16,27 +16,7 @@ interface IVotingCtx {
   incrementVote: () => void;
 }
 const defaultInitialState = {
-  parties: [
-    {
-      id: '2ab435ef-4287-4477-937a-bf7518fa061b',
-      code: 13,
-      name: 'Partido dos trabalhadores',
-      slug: 'PT',
-      votes: 0,
-      members: {
-        candidate: {
-          name: 'Joezinho da força',
-          image:
-            'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/8572bfd8-459d-4e20-8222-7ce49e67327f/joel.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220718%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220718T215045Z&X-Amz-Expires=3600&X-Amz-Signature=f06e85623442c201d923d735778ba892cff7433f095fc5eafebc7c40e0a22748&X-Amz-SignedHeaders=host&x-id=GetObject',
-        },
-        viceCandidate: {
-          name: 'Pedro Gatinho',
-          image:
-            'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/540501c6-d882-4adf-ac65-aa799e68c449/eu.jpeg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220718%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220718T215045Z&X-Amz-Expires=3600&X-Amz-Signature=7f3729ab344b4a28e1b68fd3529a6d9b47ff33383c6f12a057a07607ce9159d8&X-Amz-SignedHeaders=host&x-id=GetObject',
-        },
-      },
-    },
-  ],
+  parties: [],
   incrementVote: () => {},
 };
 
