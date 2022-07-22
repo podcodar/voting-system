a6b3965b36aa0890d80970584e90ec9a2131e98c
Guilherme barbosa <guilhermebarbo17@hotmail.com>
2022-07-22 16:39:37 -0300

    fix avatar positioning

--- packages/components/Display.tsx
+++ packages/components/Display.tsx
@@ -23,14 +23,7 @@ export default function Display() {
         <PartyInfo />
       </Flex>
       <Flex h="100%" justify="center" align="flex-end" flexDir="column">
-        <CandidatePhotos
-          candidatePhoto={
-            'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/8572bfd8-459d-4e20-8222-7ce49e67327f/joel.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220720%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220720T224244Z&X-Amz-Expires=86400&X-Amz-Signature=3290eab7bb09f8b3e65ef1b8155f078a90634b4697d7e961b594a225e0028840&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22joel.jpg%22&x-id=GetObject'
-          }
-          vicePhoto={
-            'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/8572bfd8-459d-4e20-8222-7ce49e67327f/joel.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220720%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220720T224244Z&X-Amz-Expires=86400&X-Amz-Signature=3290eab7bb09f8b3e65ef1b8155f078a90634b4697d7e961b594a225e0028840&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22joel.jpg%22&x-id=GetObject'
-          }
-        />
+        <CandidatePhotos />
       </Flex>
     </Grid>
   );
