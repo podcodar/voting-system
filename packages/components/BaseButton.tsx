5de6a94b5646861b0e9642afd04ac71bb6955010
borgesgfj <borgesgfj@gmail.com>
2022-07-27 21:40:39 -0300

    [enh] change base button design

--- packages/components/BaseButton.tsx
+++ packages/components/BaseButton.tsx
@@ -10,27 +10,27 @@ interface Props {
 
 const VARIANT_MAP: Record<
   Variant,
-  { padding: number; color: string; fontColor: string }
+  { paddingY: number; color: string; fontColor: string }
 > = {
-  confirm: { padding: 6, color: '#46F70C', fontColor: 'white' },
-  correct: { padding: 4, color: '#FFD600', fontColor: 'white' },
-  blank: { padding: 4, color: '#ffffff', fontColor: 'black' },
-  number: { padding: 4, color: '#000000', fontColor: 'white' },
+  confirm: { paddingY: 8, color: '#46F70C', fontColor: 'white' },
+  correct: { paddingY: 5, color: '#FFD600', fontColor: 'white' },
+  blank: { paddingY: 5, color: '#ffffff', fontColor: 'black' },
+  number: { paddingY: 6, color: '#000000', fontColor: 'white' },
 };
 
-export function BaseButton(props: Props) {
+export default function BaseButton(props: Props) {
   const { text, onClick, variant } = props;
-  const { color, padding, fontColor } = VARIANT_MAP[variant];
+  const { color, paddingY: paddingY, fontColor } = VARIANT_MAP[variant];
   return (
     <Button
-      marginLeft={20}
       onClick={onClick}
       bg={color}
-      padding={padding}
+      py={paddingY}
       border="1px"
       style={{ color: fontColor }}
       fontWeight="bold"
       boxShadow="md"
+      w="100%"
     >
       {text}
     </Button>
