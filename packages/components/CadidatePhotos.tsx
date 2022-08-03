9d3d874fc2e0684d03176c45b9fc81ce219a79df
Guilherme barbosa <guilhermebarbo17@hotmail.com>
2022-08-03 09:44:03 -0300

    change box to flex

--- packages/components/CadidatePhotos.tsx
+++ packages/components/CadidatePhotos.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Box, Flex, Spacer } from '@chakra-ui/react';
+import { Avatar, Flex, Spacer } from '@chakra-ui/react';
 
 interface CadidateCardProps {
   candidatePhoto?: string;
@@ -10,12 +10,12 @@ export default function CandidatePhotos({
   vicePhoto,
 }: CadidateCardProps) {
   return (
-    <Box padding="1rem">
+    <Flex padding="1rem" flexDirection="column">
       <Avatar boxSize="140px" src={candidatePhoto} />
       <Spacer height="2rem" />
       <Flex justifyContent="center">
-        <Avatar boxSize="120px" src={vicePhoto} />
+        <Avatar boxSize="120px" src={vicePhoto} marginTop="1rem" />
       </Flex>
-    </Box>
+    </Flex>
   );
 }
