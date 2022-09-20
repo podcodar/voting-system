dd765ef9f075493d6618d132ff0d08af36151b19
frattezi <pfrattezi@gmail.com>
2022-09-20 14:36:22 -0300

    demo middleware

--- middleware.ts
+++ middleware.ts
@@ -3,56 +3,63 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname === '/api/elections') {
-    if (request.method === 'GET') {
-      if (request.nextUrl.searchParams.get('databaseId') === '') {
-        return new NextResponse(null, {
-          status: 400,
-          statusText: "The databaseId parameter can't null.",
-        });
-      }
-      const paramDatabaseId: string | null =
-        request.nextUrl.searchParams.get('databaseId');
+  const { pathname } = request.nextUrl;
 
-      if (paramDatabaseId?.length !== 32) {
-        return new NextResponse(null, {
-          status: 400,
-          statusText: 'Invalid length databaseId parameter.',
-        });
-      }
-    } else {
-      return new NextResponse(null, {
-        status: 405,
-        statusText: 'Method Not Allowed.',
-      });
-    }
+  switch (pathname) {
+    case '/api/elections':
+      _electionsMiddleware(request);
+    case '/api/elections/page':
+      _electionsPageMiddleware(request);
+    default:
+      return NextResponse.next();
+  }
+}
+
+function _electionsMiddleware(request: NextRequest) {
+  if (request.method !== 'GET') {
+    return new NextResponse(null, {
+      status: 405,
+      statusText: 'Method Not Allowed.',
+    });
   }
 
-  if (request.nextUrl.pathname === '/api/elections/page') {
-    if (request.method === 'GET') {
-      if (request.nextUrl.searchParams.get('pageId') === '') {
-        return new NextResponse(null, {
-          status: 400,
-          statusText: "The pageId parameter can't null.",
-        });
-      }
+  const { searchParams } = request.nextUrl;
+  const databaseId = searchParams.get('databaseId');
 
-      const paramPageId: string | null =
-        request.nextUrl.searchParams.get('pageId');
+  if (!databaseId || databaseId.length !== 32)
+    return _invalidParameterResponse('Invalid databaseId parameter.');
+}
 
-      if (paramPageId?.length !== 36) {
-        return new NextResponse(null, {
-          status: 400,
-          statusText: 'Invalid length pageId parameter.',
-        });
-      }
-    } else {
-      return new NextResponse(null, {
-        status: 405,
-        statusText: 'Method Not Allowed.',
-      });
-    }
+function _electionsPageMiddleware(request: NextRequest) {
+  if (request.method !== 'GET') {
+    return new NextResponse(null, {
+      status: 405,
+      statusText: 'Method Not Allowed.',
+    });
   }
+
+  if (request.nextUrl.searchParams.get('pageId') === '') {
+    return new NextResponse(null, {
+      status: 400,
+      statusText: "The pageId parameter can't null.",
+    });
+  }
+
+  const paramPageId: string | null = request.nextUrl.searchParams.get('pageId');
+
+  if (paramPageId?.length !== 36) {
+    return new NextResponse(null, {
+      status: 400,
+      statusText: 'Invalid length pageId parameter.',
+    });
+  }
+}
+
+function _invalidParameterResponse(message: string) {
+  return new NextResponse(null, {
+    status: 400,
+    statusText: message,
+  });
 }
 
 // See "Matching Paths" below to learn more
