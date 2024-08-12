d280a457073407aa22aaa82dee808f869eadf692
joel-leal <joel.lasi@hotmail.com>
2024-08-12 15:02:17 -0300

    Adding a Dialog

--- src/packages/components/HomePage/ModalCreateElection.tsx
+++ src/packages/components/HomePage/ModalCreateElection.tsx
@@ -1,43 +1,37 @@
 "use client";
 import { addElection } from "@packages/DAO/elections.dao";
-import { useState } from "react";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@packages/shadcn-ui/ui/dialog";
+import Button from "../Button";
 import CreateElection from "./CreateElection";
 
 export default function ModalCreateElection() {
-  const [isOpen, setIsOpen] = useState(false);
-
   const formAction = async (formData: FormData) => {
-    setIsOpen(false);
     await addElection(formData);
   };
 
   return (
-    <>
-      {!isOpen && (
-        <button
-          type="button"
-          className="bg-transparent hover:bg-teal-500 text-teal-700 font-semibold hover:text-white py-2 px-4 border border-teal-500 hover:border-transparent rounded"
-          onClick={() => setIsOpen(!isOpen)}
-        >
-          Create Election
-        </button>
-      )}
-      {isOpen && (
-        <div className="flex justify-center items-center bg-opacity-90 top-0 left-0 z-100 fixed w-full h-full bg-gray-300">
-          <div className="flex bg-white flex-col h-50 margin-auto rounded-md border-0">
-            <button
-              type="button"
-              className="flex pr-4 pt-3 text-sm justify-end font-semibold text-gray-300 hover:text-gray-500"
-              onClick={() => setIsOpen(false)}
-            >
-              x
-            </button>
-            <div>
-              <CreateElection onAction={formAction} />
-            </div>
-          </div>
-        </div>
-      )}
-    </>
+    <Dialog>
+      <DialogTrigger>
+        <Button>Criar Eleição</Button>
+      </DialogTrigger>
+
+      <DialogContent className="bg-zinc-50">
+        <DialogHeader>
+          <DialogTitle>Crie uma eleição</DialogTitle>
+
+          <DialogDescription>
+            <CreateElection onAction={formAction} />
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
   );
 }
