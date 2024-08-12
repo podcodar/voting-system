37c85fe9db570b83fca64666dad56fbf246ff6ea
joel-leal <joel.lasi@hotmail.com>
2024-08-12 15:07:41 -0300

    Redirecting Election Routes with ID

---
+++ src/app/elections/page.tsx
@@ -0,0 +1,14 @@
+import ElectionForm from "@packages/components/HomePage/ElectionForm";
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex items-center justify-center h-[80vh] content-center">
+      <div>
+        <p> Elections</p>
+      </div>{" "}
+    </div>
+  );
+};
+
+export default Home;
