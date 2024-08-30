2d216283cad419503346535f9ba4619741154b38
joel-leal <joel.lasi@hotmail.com>
2024-08-30 11:46:05 -0300

    link home in navBar

--- src/packages/components/NavBar.tsx
+++ src/packages/components/NavBar.tsx
@@ -1,10 +1,13 @@
+import Link from "next/link";
 import ModalCreateElection from "./HomePage/ModalCreateElection";
 
 export default function NavBar() {
   return (
     <div className="sticky top-0 z-10 bg-white shadow-lg">
       <div className="flex container mx-auto p-4 content-between md:container md:mx-auto items-center justify-between">
-        <h1 className="text-teal-500 font-bold">Voting System</h1>
+        <Link href="/">
+          <h1 className="text-teal-500 font-bold">Voting System</h1>
+        </Link>
         <ModalCreateElection />
       </div>
     </div>
