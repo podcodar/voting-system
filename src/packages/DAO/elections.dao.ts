5c74f8e06ca6cf52a010eebd2a78f24585542f85
joel-leal <joel.lasi@hotmail.com>
2024-09-18 08:47:35 -0300

    feat: creating error class

--- src/packages/DAO/elections.dao.ts
+++ src/packages/DAO/elections.dao.ts
@@ -3,10 +3,16 @@
 import prisma from "@lib/prisma";
 import { createElectionValidator } from "@packages/dto/elections.dto";
 import type { CreateElection } from "@packages/entities/elections";
+import { BadRequestError, NotFound } from "@packages/utils/error";
 import { ElectionStatus } from "@prisma/client";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { z } from "zod";
+
+interface FindElection {
+  name: string;
+  status: string;
+}
 
 export async function getElectionsOptions() {
   const elections = await getElections();
@@ -51,19 +57,39 @@ export async function startElection(formData: FormData) {
   redirect(`http://localhost:3000/elections/${electionId}`);
 }
 
+async function getElections() {
+  const res = await prisma.election.findMany();
+  return res;
+}
+
+async function findByNameOrStatus({ name, status }: FindElection) {
+  const resultElection = await getElections();
+  console.log(resultElection);
+  const result = resultElection.find(
+    (e) => e.name === name && e.status === status,
+  );
+  return result;
+}
+
 export async function addElection(formData: FormData) {
   const data = Object.fromEntries(formData);
-  const parsedData = createElectionValidator.parse(data);
-  await prisma.election.create({
-    data: parsedData,
-  });
+  const parsedData = createElectionValidator.safeParse(data); // Error empty input
+  if (!parsedData.success) {
+    throw new BadRequestError(parsedData.error.name);
+  }
+  const electionExist = await findByNameOrStatus(parsedData.data); // Error duplicate data
+  if (electionExist) {
+    throw new BadRequestError(parsedData.data.name); //fazer correção no AppError
+  }
 
+  await prisma.election.create({
+    data: parsedData.data,
+  });
   revalidatePath("/");
-}
-async function getElections() {
-  return await prisma.election.findMany();
 }
 
 // async function updateElection() {
 //   return await prisma.election.findMany();
 // }
+
+console.error;
---
+++ src/packages/utils/error.ts
@@ -0,0 +1,21 @@
+// Clicar no botão para criar uma eleição sem dados - Error 1
+
+// Clicar no botão de iniciar eleição sem selecionar eleição - Error 2
+
+export class NotFound extends Error {
+  public readonly statusCode: number;
+
+  constructor(entity: string) {
+    super(`${entity} não foi encontrada`);
+    this.statusCode = 404;
+  }
+}
+
+export class BadRequestError extends Error {
+  public readonly statusCode: number;
+
+  constructor(data: string) {
+    super(data);
+    this.statusCode = 400;
+  }
+}
