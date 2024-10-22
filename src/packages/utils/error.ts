7a991c78dd714d12c15d454d73a842420dc1bf30
joel-leal <joel.lasi@hotmail.com>
2024-10-22 12:26:15 -0300

    fix: corrected error message

--- src/packages/utils/error.ts
+++ src/packages/utils/error.ts
@@ -11,7 +11,7 @@ export class BadRequestError extends Error {
   public readonly statusCode: number;
 
   constructor(data: string) {
-    super(data);
+    super(`Este erro ${data} foi encontrado`);
     this.statusCode = 400;
   }
 }
