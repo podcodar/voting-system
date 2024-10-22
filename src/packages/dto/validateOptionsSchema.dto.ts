612983bf43d4052ff6fd484a1995dba2bd92b7c1
joel-leal <joel.lasi@hotmail.com>
2024-10-22 11:27:28 -0300

    fix: Deleted file validateOptionsSchema without functionality

--- src/packages/dto/validateOptionsSchema.dto.ts
+++
@@ -1,6 +0,0 @@
-import { z } from "zod";
-
-export const validateOptionsSchema = z.object({
-  name: z.string().min(1, { message: "" }).max(20, ""),
-  value: z.string().min(1, { message: "" }).max(20, ""),
-});
