bd89ef9645bddf3afe075448608810c1ce3e07c7
joel-leal <joel.lasi@hotmail.com>
2024-10-23 11:15:49 -0300

    fix: Applying corrections from the review and refactoring the error messages.

--- src/packages/dto/elections.dto.ts
+++ src/packages/dto/elections.dto.ts
@@ -4,14 +4,18 @@ import { z } from "zod";
 export const createElectionValidator = z.object({
   name: z
     .string()
-    .min(5, "O nome de usuário deve ter no mínimo 5 caracteres")
-    .max(20, "O nome de usuário deve ter no máximo 20 caracteres"),
+    .min(5, { message: "O nome de usuário deve ter no mínimo 5 caracteres" })
+    .max(20, { message: "O nome de usuário deve ter no máximo 20 caracteres" }),
   status: z.nativeEnum(ElectionStatus),
 });
 
 export const electionSchema = z.object({
   name: createElectionValidator.shape.name,
-  candidates: z.array(z.string().uuid()),
+  candidates: z.array(z.string().uuid({ message: "ID de candidato inválido" })),
 });
 
-export const updateElectionSchema = z.string().uuid();
+export const updateElectionSchema = z
+  .string()
+  .uuid({ message: "ID de eleição inválido" });
+
+type FindElection = z.infer<typeof createElectionValidator>;
