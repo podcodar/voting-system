c1f2b5d9da0fe6084a7d5cf367d95e5b520cc47c
joel-leal <joel.lasi@hotmail.com>
2024-10-24 13:42:52 -0300

    fea: Created election result page

---
+++ src/app/elections/[electionId]/results/page.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+
+const results: NextPage = () => {
+  return (
+    <div className="flex items-center justify-center h-[80vh] content-center">
+      <div>
+        <p> Election Results </p>
+      </div>{" "}
+    </div>
+  );
+};
+
+export default results;
