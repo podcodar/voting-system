6a0d66d2895785126f66060a1cff8078c687a416
joel-leal <joel.lasi@hotmail.com>
2024-10-28 11:51:00 -0300

    fea: created result page for rendering result cardas

---
+++ src/app/elections/[electionId]/results/page.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div className="flex items-center justify-center h-[80vh] content-center">
+      <div>
+        <p> aqui ficará os cards de resultado</p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
