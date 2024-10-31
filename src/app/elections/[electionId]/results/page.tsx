cf9a22b2ec908bb65a1d55a579fd9835dc39ecc1
joel-leal <joel.lasi@hotmail.com>
2024-10-31 12:44:45 -0300

    fea: create result card and add dependencies avatar

--- src/app/elections/[electionId]/results/page.tsx
+++ src/app/elections/[electionId]/results/page.tsx
@@ -1,8 +1,18 @@
+import ResultCard from "@packages/components/ResultCard";
+
 const Home = () => {
   return (
     <div className="flex items-center justify-center h-[80vh] content-center">
       <div>
         <h1> Resultados da Eleição </h1>
+        <ResultCard
+          image="/image/avatar1.png"
+          candidate="João Silva"
+          vice="Maria Oliveira"
+          party=" XYZ"
+          percentagem="58%"
+          votos="3,546"
+        />
       </div>
     </div>
   );
