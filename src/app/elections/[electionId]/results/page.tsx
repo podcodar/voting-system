b74c7a117c531a3c2095af5666d4fb9b2747929e
joel-leal <joel.lasi@hotmail.com>
2024-10-31 16:45:29 -0300

    fea: test inserted data and avatar in result card

--- src/app/elections/[electionId]/results/page.tsx
+++ src/app/elections/[electionId]/results/page.tsx
@@ -1,18 +1,26 @@
 import ResultCard from "@packages/components/ResultCard";
-
+import candidates from "src/data/data";
 const Home = () => {
   return (
-    <div className="flex items-center justify-center h-[80vh] content-center">
-      <div>
-        <h1> Resultados da Eleição </h1>
-        <ResultCard
-          image="/image/avatar1.png"
-          candidate="João Silva"
-          vice="Maria Oliveira"
-          party=" XYZ"
-          percentagem="58%"
-          votos="3,546"
-        />
+    <div className="p-20">
+      <h1 className="text-center text-xl">Resultados da Eleição</h1>
+      <div className="flex items-center justify-center h-[80vh] content-center">
+        <div>
+          {candidates.map(
+            ({ image, candidate, vice, party, percentagem, votos }) => (
+              <div key={candidate} className="p-4">
+                <ResultCard
+                  image={image}
+                  candidate={candidate}
+                  vice={vice}
+                  party={party}
+                  percentagem={percentagem}
+                  votos={votos}
+                />
+              </div>
+            ),
+          )}
+        </div>
       </div>
     </div>
   );
---
+++ src/data/data.tsx
@@ -0,0 +1,44 @@
+const candidates = [
+  {
+    image: "/image/avatar5.png",
+    candidate: "Jiji Ping Png",
+    vice: "Xinguilingui",
+    party: "PCC",
+    percentagem: "35.2%",
+    votos: "3,200,000",
+  },
+  {
+    image: "/image/avatar4.png",
+    candidate: "Vladimir Putin",
+    vice: "Zelensky",
+    party: "ABC",
+    percentagem: "28.1%",
+    votos: "2,550,000",
+  },
+  {
+    image: "/image/avatar3.png",
+    candidate: "Lulão da massa",
+    vice: "Xuxu",
+    party: "DEF",
+    percentagem: "18.4%",
+    votos: "1,670,000",
+  },
+  {
+    image: "/image/avatar2.png",
+    candidate: "Biden",
+    vice: "Gagá",
+    party: "GHI",
+    percentagem: "12.3%",
+    votos: "1,120,000",
+  },
+  {
+    image: "/image/avatar1.png",
+    candidate: "Trump",
+    vice: "Bolsonaro",
+    party: "JKL",
+    percentagem: "6.0%",
+    votos: "540,000",
+  },
+];
+
+export default candidates;
